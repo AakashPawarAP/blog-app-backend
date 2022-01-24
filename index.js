a151ca2183e78cc22467c03d07f4c2478375c2a9
AakashPawarAP <aakashpawar97@gmail.com>
2022-01-24 16:19:54 +0530

    removed images

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use("/images", express.static("images"));
+// app.use("/images", express.static("images"));
 app.use(express.json());
 app.use(cors());
 const blogRouter = require("./routes/allBlogs");
@@ -9,7 +9,7 @@ const homeArticles = require("./routes/homeArticles");
 const { registerRouter } = require("./routes/registerUser");
 const loginRouter = require("./routes/login");
 const topPosts = require("./routes/topPosts");
-// const blogRouter = createRequire("./api-routes/allBlogs");
+// const blogRouter = createRequire("./routes/allBlogs");
 
 app.use("/api/v1/blogs", blogRouter);
 app.use("/api/v1/home-articles", homeArticles);
