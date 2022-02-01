207ad8624b9a0c6a9b2ea600735a22b4616a0a18
AakashPawarAP <aakashpawar97@gmail.com>
2022-02-01 15:38:10 +0530

    changes in heading and desc

--- blogs/homeArticles.js
+++ blogs/homeArticles.js
@@ -1,36 +1,32 @@
 const articlesContainer = [
   {
     img: "https://images.indianexpress.com/2021/10/exercise_1200_getty.jpg",
-    blogHead: "Fitness alert: Find out if you should workout when sick",
-    blogDesc:
-      "Many fitness enthusiasts continue to exercise even on days they do not feel well thinking that not working out may put a dent on their goals.",
+    heading: "Fitness alert: Find out if you should workout when sick",
+    desc: "Many fitness enthusiasts continue to exercise even on days they do not feel well thinking that not working out may put a dent on their goals.",
     type: "Fitness",
     date: " / Aug 11 2021",
   },
   {
     img: "https://images.indianexpress.com/2022/01/6g-jio-featured.jpg",
-    blogHead:
+    heading:
       "Jio partners with University of Oulu over development of 6G technology",
-    blogDesc:
-      "Reliance Jio’s Estonia unit, (Jio Estonia OÜ) is partnering with the University of Oulu for exploring 6G, the upcoming next generation of telecom technology after 5G.",
+    desc: "Reliance Jio’s Estonia unit, (Jio Estonia OÜ) is partnering with the University of Oulu for exploring 6G, the upcoming next generation of telecom technology after 5G.",
     type: "Technology",
     date: " / Jan 01 2021",
   },
   {
     img: "https://i.gadgets360cdn.com/large/tesla_elon_musk_reuters_1640604041126.jpg?downsize=950:*",
-    blogHead:
+    heading:
       "Elon Musk Says Twitter Introducing NFT Profile Pictures Is 'Annoying'",
-    blogDesc:
-      "Elon Musk doesn't appear to be amused with Twitter's move to let users have verified non-fungible tokens (NFTs) as profile pictures.",
+    desc: "Elon Musk doesn't appear to be amused with Twitter's move to let users have verified non-fungible tokens (NFTs) as profile pictures.",
     type: "Technology",
     date: " / August 21 2021",
   },
   {
     img: "https://imagevars.gulfnews.com/2016/3/12/1_16a081427ab.1688756_1715675177_16a081427ab_medium.jpg",
-    blogHead:
+    heading:
       "Significant reshoots underway for Doctor Strange in the 'Multiverse of Madness'",
-    blogDesc:
-      "The Benedict Cumberbatch starring ‘Doctor Strange in the Multiverse of Madness’ has been prescribed a major new shoot.",
+    desc: "The Benedict Cumberbatch starring ‘Doctor Strange in the Multiverse of Madness’ has been prescribed a major new shoot.",
     type: "Hollywood",
     date: " / Dec 30 2021",
   },
